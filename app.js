5d2710b5145a02353ea0f46116f6e35148380c6b
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-03-03 18:58:45 +0200

    changed package.json

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const logger = require('morgan');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
 const errorHandler = require('./middleware/error');
+const { dirname } = require('path');
 
 // connect db
 connectDB();
@@ -31,6 +32,18 @@ app.use('/api/private', require('./routes/private'));
 
 // Error Handler (Should be last piece of middleware)
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up assets
+  app.use(express.static('client-admin/build'));
+
+  // Express will server index.html file if it doesn't recognize the route
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client-admin', 'build', 'index.html'));
+  });
+}
+
+
 
 // app.use(express.static(path.join(__dirname, './client-admin', 'build')));
 // app.use('/', function(req, res) {
